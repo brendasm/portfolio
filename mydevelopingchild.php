fd13dd83fb56172b85c66525b51adbccf5933bc3
brendasm <Little@Brendas-Air.T-mobile.com>
2017-05-04 02:28:12 -0700

    Edits 5/3 end of day
    mydevelopingchild.php

--- mydevelopingchild.php
+++ mydevelopingchild.php
@@ -10,15 +10,51 @@
 
 	<?php include 'include/about.php'; // include about me section ?>
 
-	<section id="hero-image" class="mdc">
+		<section id="hero-image" class="mdc">
+			<div class="hero-filter">
+			<?php include 'include/nav.php'; // include nav ?>
+			</div> <!-- end hero filter -->	
+		</section> <!-- end hero image -->
 
-		<div class="hero-filter">
 
-		<?php include 'include/nav.php'; // include nav ?>
 
-		</div> <!-- end hero filter -->
-		
-	</section>
+
+
+	<main id="content">
+<!-- 		<div class="wrapper">	 -->
+
+		<section id="showcase">
+
+		<h2 class="title">My Developing Child</h2>
+
+		<div class="skills-wrapper">
+		<ul class="skills">
+			<li>UX Design</li>
+			<li>UI Design</li>
+			<li>WordPress Custom Theme</li>
+			<li>Information Architecture</li>
+			<li>Branding</li>
+			<li>Responsive Design</li>
+			<li>PHP</li>
+			<li>Sass</li>
+			<li>Git</li>
+			<li>User Testing</li>
+		</ul>
+		</div>	
+
+
+
+									<p>My Developing Child is an educational website for a non-profit organization that focuses on bringing awareness to autism. The main requirements for the design and development of this project was to create a clean and easy to navigate site. With a user centered approach, we incorporated various dynamic elements to enhance the overall usability of the site with specific functions enabled/disabled at various screen sizes. </p>
+
+								<p>The project was a collaborative effort with two other team members. </p>
+
+<div class="divider div-transparent div-arrow-down"></div>
+
+			
+		</section> <!-- end showcase -->
+
+<!-- 		</div> --> <!-- end wrapper -->
+	</main>	
 
 
 	<?php include 'include/footer.php'; // include footer ?>
