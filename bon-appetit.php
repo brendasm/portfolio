49de02d6f251bf9e872086f1d6e9078090afc1a5
brendasm <Little@Brendas-Air.T-mobile.com>
2017-05-04 23:00:26 -0700

    edits to bon-appetit.php
    added description of project and photos

--- bon-appetit.php
+++ bon-appetit.php
@@ -10,15 +10,56 @@
 
 	<?php include 'include/about.php'; // include about me section ?>
 
-	<section id="hero-image" class="bonapp">
+		<section id="hero-image" class="bonapp">
+		<div class="hero-filter">
+			<?php include 'include/nav.php'; // include nav ?>		
+			</div> <!-- end hero filter -->		
+		</section> <!-- end hero image -->
 
-	<div class="hero-filter">
+	<main id="content">
+		<div class="showcase-wrapper">
 
-		<?php include 'include/nav.php'; // include nav ?>
-		
-		</div> <!-- end hero filter -->
-		
-	</section>
+		<section id="showcase">
+
+		<h2 class="title">Bon App&Eacute;tit</h2>
+
+		<div class="skills-wrapper">
+		<ul class="skills">
+			<li>User Experience Design</li>
+			<li>Responsive Design</li>
+			<li>Mobile First Design</li>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>jQuery</li>
+		</ul>
+		</div> <!-- skills wrapper -->
+
+		<div class="description">
+		<p>The objective of this project was to create a one-page responsive site that is compatible across all devices – desktop, tablet and mobile. This project ties together two things I am passionate about: good food & responsive design. I designed this project with flexbox to create a masonry grid layout (inspired by pinterest) which is based on columns that don’t have fixed height rows. I used a lightbox to showcase the images. With a responsive design approach, I disabled the lightbox on specific screen sizes and switched in a responsive navigation bar to bring together a functional user experience.</p>
+		</div> <!-- end description -->
+
+		<a href="/projects/responsive/" class="btn-project-pg" title="View Project" target="_blank">View Live</a>
+
+		<div class="border"></div>
+
+		<div class="description">
+		<p>I incorporated colorful food imagery to bring out the vibrant theme of this project. I also used a hover effect on the images so that users would be inclined to click on the photos. </p>	
+		</div> <!-- end description -->
+
+		<div class="photo-wrapper">
+
+			<div class="photo">
+				<img src="images/project/responsive-2.jpg" alt="" title="">
+			</div>
+
+		</div> <!-- end photo wrapper -->
+
+
+			
+		</section> <!-- end section showcase -->
+
+		</div> <!-- end showcase wrapper -->
+	</main>	
 
 
 	<?php include 'include/footer.php'; // include footer ?>
--- jquery-tutorial.php
+++ jquery-tutorial.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Brenda Lau | Jquery Project
- </title>
+	<title>Brenda Lau | Jquery Project
+	</title>
 	<?php include 'include/header.php'; // includes all header content ?>
 </head>
 
@@ -11,73 +11,70 @@
 
 	<?php include 'include/about.php'; // include about me section ?>
 
-		<section id="hero-image" class="jquery">
-			<div class="hero-filter">
+	<section id="hero-image" class="jquery">
+		<div class="hero-filter">
 			<?php include 'include/nav.php'; // include nav ?>
-			</div> <!-- end hero filter	 -->		
-		</section> <!-- end hero image -->
+		</div> <!-- end hero filter	 -->		
+	</section> <!-- end hero image -->
 
 	<main id="content">
 		<div class="showcase-wrapper">
 
-		<section id="showcase">
+			<section id="showcase">
 
-			<h2 class="title">Jquery Tutorial</h2>
+				<h2 class="title">Jquery Tutorial</h2>
 
-			<div class="skills-wrapper">
-			<ul class="skills">
-				<li>Visual Design</li>
-				<li>HTML</li>
-				<li>CSS</li>
-				<li>jQuery</li>
-			</ul>
-			</div>
+				<div class="skills-wrapper">
+					<ul class="skills">
+						<li>Visual Design</li>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>Javascript / jQuery</li>
+					</ul>
+				</div>
 
-			<div class="description">
-			<p>The goal of this project was to create a site to showcase the use of javascript / jQuery in three tutorials. The tutorials are focused on an imagery theme with step-by-step instructions to incorporate jQuery image effects on any site. The project is designed to be clean, concise and easy for users to follow through each tutorial.</p>
+				<div class="description">
+					<p>The goal of this project was to create a site to showcase the use of javascript / jQuery in three tutorials. The tutorials are focused on an imagery theme with step-by-step instructions to incorporate jQuery image effects on any site. The project is designed to be clean, concise and easy for users to follow through each tutorial.</p>
 
-			<p>Since this is an earlier project, responsive design is not implemented. Project is best viewed on browsers with screen sizes 1100px and up.</p>
-			</div> <!-- end description -->
+					<p>Since this is an earlier project, responsive design is not implemented. Project is best viewed on browsers with screen sizes 1100px and up.</p>
+				</div> <!-- end description -->
 
-			<a href="/projects/jquery-tutorial/" class="btn-project-pg" title="View Project" target="_blank">View Live</a>
+				<a href="/projects/jquery-tutorial/" class="btn-project-pg" title="View Project" target="_blank">View Live</a>
 
-			<div class="border"></div>
+				<div class="border"></div>
 
-			<div class="description">
-			<p>The first tutorial showcases an image slider, the second one an image zoom effect. Both tutorials are to be used in conjunction with embedded code snippets and a plugin. Documentation is provided for the plugins to allow for easy implementation. Finally, the last tutorial is also an image slider which shows line by line the written script required to function. The site also includes a sticky navbar to allow easy navigation between tutorials. 
-			</p>
-			<p>Since the step-by-step documentation is text heavy, I used accordion tabs to hide/show information as required. </p>
-			</div> <!-- end description -->
+				<div class="description">
+					<p>The first tutorial showcases an image slider, the second one an image zoom effect. Both tutorials are to be used in conjunction with embedded code snippets and a plugin. Documentation is provided for the plugins to allow for easy implementation. Finally, the last tutorial is also an image slider which shows line by line the written script required to function. The site also includes a sticky navbar to allow easy navigation between tutorials. 
+					</p>
+					<p>Since the step-by-step documentation is text heavy, I used accordion tabs to hide/show information as required. </p>
+				</div> <!-- end description -->
 
-			<div class="swatch">
-			<div class="swatch-single jquery1"><p>#FFEFD5</p></div>
-			<div class="swatch-single jquery2"><p>#ADD8E6</p></div>
-			<div class="swatch-single jquery3"><p>#A9A9A9</p></div>
-			</div> <!-- end swatch -->
+				<div class="swatch">
+					<div class="swatch-single jquery1"><p>#FFEFD5</p></div>
+					<div class="swatch-single jquery2"><p>#ADD8E6</p></div>
+					<div class="swatch-single jquery3"><p>#A9A9A9</p></div>
+				</div> <!-- end swatch -->
 
-			<div class="description">
-			<p>I incorporated three light and warm colors to bring out a tropical theme for the site. </p>
-			</div>
+				<div class="description">
+					<p>I incorporated three light and warm colors to bring out a tropical theme for the site. </p>
+				</div>
 
-			<div class="photo-wrapper">
-			<div class="photo">
-				<img src="images/project/tutorial-1.png" alt="jquery tutorial screenshot" title="jquery tutorial screenshot">
-			</div>
-			<div class="photo">
-				<img src="images/project/tutorial-2.png" alt="jquery tutorial screenshot" title="jquery tutorial screenshot">				
-			</div>
-			</div> <!-- end photo wrapper -->
+				<div class="photo-wrapper">
+					<div class="photo">
+						<img src="images/project/tutorial-1.png" alt="jquery tutorial screenshot" title="jquery tutorial screenshot">
+					</div>
+					<div class="photo">
+						<img src="images/project/tutorial-2.png" alt="jquery tutorial screenshot" title="jquery tutorial screenshot">				
+					</div>
+				</div> <!-- end photo wrapper -->
 
 
-
-		</section> <!-- end section showcase -->
+			</section> <!-- end section showcase -->
 
 
 
 		</div> <!-- end showcase wrapper -->
 	</main>
-
-
 
 
 	<?php include 'include/footer.php'; // include footer ?>
