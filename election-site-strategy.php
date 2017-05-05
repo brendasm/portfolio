8e265cd9f2114ee09cd2bcdc379969ae8c08abcf
brendasm <Little@Brendas-Air.T-mobile.com>
2017-05-05 10:56:08 -0700

    election-site-strategy final edits

--- election-site-strategy.php
+++ election-site-strategy.php
@@ -37,7 +37,7 @@
 
 		<div class="description">
 		<p>The scope of this UX project was to create a content strategy proposal to redesign the ‘Elections Canada’ website. The redesign aims to deliver an organized site architecture that simplifies the process of voting and improves the current user experience from beginning to end. The design incorporates a clean and effective layout that is easy for all types of users to navigate for information. </p>
-		<p>The project was separated in two parts – Content Strategy and Prototype.  The content strategy was developed in a collaborative effort of five team members. The individual component consisted of creating a functioning prototype, style guide and style tile.</p>	
+		<p>The project was separated in two parts – Content Strategy and Prototype.  The content strategy was developed in a collaborative effort of five team members. The individual component consisted of creating a functioning prototype, style guide and style tile. View the below pdf strategy for more details.</p>	
 		</div> <!-- end description -->
 
 		<a href="/projects/elections-canada-ux-project.pdf" class="btn-project-pg" title="View Project" target="_blank">View Strategy</a>
@@ -46,8 +46,24 @@
 
 		<div class="description">
 		<p><span class="bold">Content Strategy</span> &nbsp; For the strategy, we first began with a design sprint to generate as many ideas as possible in order to understand, define and decide on the direction of our design. From there, we established goals, target audiences and interviewed users to develop fact based personas and empathy maps. The strategy also includes an information architecture and competitive gap analysis where the voting websites of four countries were compared against ‘Elections Canada’. </p>	
+		<p><span class="bold">Proposal</span> The proposal brings together key elements to redesign the current Elections Canada website with a focus on the “How to Vote” section. The proposed redesign would focus on a clean, modern approach and would include the change of fonts, colors and layout of the overall site. In addition, the use of more images will provide a better visual reference and appeal to more users visiting the site.  </p>
 		</div>
 
+		<div class="photo-wrapper">
+			<div class="photo-reg">
+				<img src="images/project/ux-project-3.png" alt="Elections Canada Screenshot" title="Elections Canada Screenshot">
+			</div>			
+			<div class="photo">
+				<img src="images/project/ux-project-1.jpg" alt="Elections Canada Style Guide" title="Elections Canada Style Guide">
+			</div>
+			<div class="photo">
+				<img src="images/project/ux-project-2.jpg" alt="Elections Canada Style Tile" title="Elections Canada Style Tile">
+			</div>						
+		</div> <!-- end photo wrapper -->
+
+		<div class="description">
+		<p><span class="bold">Style Guide and Tile</span> The style tile and guide brings together a visual reference to the design language of the site and also includes the voice and tone the site should convey. </p>
+		</div>
 
 			
 		</section> <!-- end section showcase -->
