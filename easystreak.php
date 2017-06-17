808b70a3131c65483493b48c9a8c58f6e6474110
brendasm <Little@Brendas-Air.T-mobile.com>
2017-06-16 17:29:49 -0700

    updates to nav

--- easystreak.php
+++ easystreak.php
@@ -1,1 +1,6 @@
-easystreak.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Brenda Lau | Easy Streak Project </title>
+	<?php include 'include/header.php'; // includes all header content ?>
+</head>
--- include/nav.php
+++ include/nav.php
@@ -1,5 +1,5 @@
 <nav class="nav-main">
-	<div class="logo"><a href="index.php#home">Brendalau.com</a></div>
+	<div class="logo"><a href="index.php">Brendalau.com</a></div>
 	<ul class="nav-bar">
 		<li><a href="index.php">Home</a></li>
 		<li><a href="index.php#portfolio">Work</a></li>
