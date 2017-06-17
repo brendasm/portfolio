dfa89268008fa696dff9e1b79a526d5bfdac9fc2
brendasm <Little@Brendas-Air.T-mobile.com>
2017-06-16 19:05:27 -0700

    update easy streak

--- easystreak.php
+++ easystreak.php
@@ -32,10 +32,50 @@
 		</div> <!-- end skills wrapper -->
 
 		<div class="description">
-			<p>Easy streak is a gaming app that offers sports fans a platform to compete in daily sport contests/pools where they are able to select the winning teams of NBA, NHL, MLB, NFL and MLS games to win cash prizes. The platform allows users to select teams in a “pick’em” format instead of selecting against a spread or betting on the money line. </p>
+			<p>Easy streak is a gaming app that offers sports fans a platform to compete in daily sport contests/pools where they are able to select the winning teams of NBA, NHL, MLB, NFL and MLS games to win cash prizes. The platform allows users to select teams in a “pick’em” format instead of selecting against a spread or betting on the money line. For more information, visit the <a href="http://www.easystreak.com/" title="Easy Streak Website" target="_blank">Easy Streak</a> website. </p>
 
-			<p><span class="bold">Project Scope and Goals</span></p>
-		</div>
+			<p><span class="bold">Project Scope &amp; Goals</span>&nbsp;I was connected with the founder of Easy Streak to redesign the user interface, visual design and improve the overall user experience of the gaming app. The app redesign aims to achieve two main goals: increase the user adoption rate and provide a trustworthy and valuable experience for users. </p>
+		</div> <!-- end description -->
+
+		<div class="border"></div>
+
+		<div class="description">
+			<p><span class="bold">Research</span>&nbsp;In order to tackle this project, I had to first research and figure out the answers to the following questions: </p>
+
+			<ul class="bullet">
+				<li>Who are the users and what are their priorities when using this product?</li>
+				<li>What are the use cases of this product?</li>
+				<li>What are the opportunities for design enhancements related to product desirability (utility, usability, and delight)?</li>
+			</ul>
+
+			<p>I worked closely with the founder and stakeholders to understand their goals and directions and also conducted phone interviews with real users that are currently using the app.</p>
+
+			<p>15 participants were interviewed to yield the following information: </p>
+
+			<ul class="bullet-list">
+				<li>The average age of users are 20-37 years old.</li>
+				<li>Users are sports fans and have participated in or have knowledge of fantasy sports. However, these users have moved away from fantasy sports because it takes too much time and commitment and may not be able to have any winnings.</li>
+				<li>On average, users prefer to use a platform that is easy to navigate without any complexities and enjoys the overall scope of choosing teams instead of players. </li>
+				<li>Users are motivated to use the app to win cash prizes, enhanced game experience, and to test whether they know which team will win</li>
+				<li>Would like to see more transparency between the scores (ie show tie breaker score) and also have updated scores in real time instead of just the final scores after all games have finished. </li>
+				<li>Users spend approximately 10-20 minutes to research sport teams and current news</li>
+				<li>On average, users may use app twice a day to find out scores and select teams in contests.</li>
+				<li>Users enjoy interacting with a simple interface that can display more games and a more streamline process when selecting teams.</li>
+				<li>More gaming options (instead of just picking teams) may increase engagement rate of using app.</li>
+			</ul>
+
+			<p>Note: Selected participants are a mix of try-outs (free), semi-pro and premium subscribers. </p>
+		</div> <!-- end description -->
+
+		<div class="description">
+			<p><span class="bold">UX Strategy</span></p>
+		</div> <!-- end description -->
+
+		<div class="photo-wrapper">
+			<div class="photo-reg">
+				<img src="images/project/easystreak/es-sitemap.png" alt="Elections Canada Screenshot" title="Elections Canada Screenshot">
+			</div>		
+		</div> <!-- end photo wrapper -->
 
 
 
